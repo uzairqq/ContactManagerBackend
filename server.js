396084ff3ec0d222921da73f59ed87c409cbfcc4
Uzair Iqbal <uzair.qq@outlook.com>
2023-07-17 21:32:15 -0700

    setup environment variables.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`I am Listening on Port`, PORT);
