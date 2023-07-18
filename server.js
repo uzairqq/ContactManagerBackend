7c8b9ba01115b51931769dd0bfc0a56fd62fdbbc
Uzair Iqbal <uzair.qq@outlook.com>
2023-07-17 22:05:55 -0700

    Created Routes Folder for Creating more routes in that. using app.use for route

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const contactRoutes = require("./routes/contactRoutes");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/api/contacts", (req, res) => {
-  res.status(200).json({ message: "Get All Contacts" });
-});
+app.use("/api/contacts", contactRoutes);
 
 app.listen(PORT, () => {
   console.log(`I am Listening on Port`, PORT);
